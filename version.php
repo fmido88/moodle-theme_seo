8df8d963bd7c6aadd42551f05a33791b6347adc8
Mohammad Farouk <phun.for.physics@gmail.com>
2026-04-11 20:41:05 +0200

    pump up the version

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_seo';
 
 // This is the version of the plugin.
-$plugin->version = 2026041100;
+$plugin->version = 2026041110;
 
 // This is the named version.
-$plugin->release = '2.0.0';
+$plugin->release = '2.0.1';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_BETA;
